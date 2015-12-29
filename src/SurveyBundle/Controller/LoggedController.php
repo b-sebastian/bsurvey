63955f532ff9b7f48c16e9705927f7ba073dde99
Sebastian Bachorczyk <sebastian.bachorczyk@gmail.com>
2015-12-29 18:14:31 +0100

    created overview page

--- src/SurveyBundle/Controller/LoggedController.php
+++ src/SurveyBundle/Controller/LoggedController.php
@@ -13,6 +13,40 @@ class LoggedController extends Controller
 	 */
 	public function overviewAction(Request $request)
 	{
-		return $this->render('SurveyBundle::overview.html.twig');
+		$manager = $this->getDoctrine()->getManager();
+
+		$surveys = $manager->getRepository('SurveyBundle:Survey')->findBy(array(
+			'user' => $this->getUser(),
+			'deleted' => false
+		));
+
+		$sessions = $manager->getRepository('SurveyBundle:Session')->createQueryBuilder('se')
+			->leftJoin('se.survey', 'su')
+			->where('su.user = :user')
+			->setParameter('user', $this->getUser())
+			->orderBy('se.fromDate', 'DESC')
+			//->setMaxResults(10)
+			->getQuery()->getResult();
+
+		$last_answer = $manager->getRepository('SurveyBundle:Answer')->createQueryBuilder('a')
+			->leftJoin('a.question', 'q')
+			->where('q.user = :user')
+			->setParameter('user', $this->getUser())
+			->setMaxResults(1)
+			->orderBy('a.id', 'DESC')
+			->getQuery()->getResult();
+
+		$thumbs = array(
+			'last_answer' => $last_answer[0],
+			'survey_count' => count($surveys),
+			'session_count' => count($sessions)
+		);
+
+		$tables = array(
+			'surveys' => $surveys,
+			'sessions' => $sessions
+		);
+
+		return $this->render('SurveyBundle::overview.html.twig', array('thumbs' => $thumbs, 'tables' => $tables));
 	}
 }
